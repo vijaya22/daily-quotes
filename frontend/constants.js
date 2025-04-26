3785af83f3271a49f322f22a25cf7e32f99e58fa
vijaya22 <vijaya2798@gmail.com>
2025-04-26 12:51:06 +0530

    change background daily

---
+++ frontend/constants.js
@@ -0,0 +1,9 @@
+window.background = [
+    "url('./assets/9446299.jpg')",
+    "url('./assets/3040791.jpg')",
+    "url('./assets/flowers-9409674.jpg')",
+    "url('./assets/flowers.jpeg')",
+    "url('./assets/flowery.jpg')",
+    "url('./assets/lotusflower.jpg')",
+    "url('./assets/vectoryflower.jpg')",
+  ];
