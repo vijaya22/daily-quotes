4eb65e3511c6473822c577d49309324b6ee22f09
vijaya22 <vijaya2798@gmail.com>
2025-04-26 15:18:34 +0530

    add more backgrounds

--- frontend/constants.js
+++ frontend/constants.js
@@ -6,4 +6,7 @@ window.background = [
     "url('./frontend/assets/flowery.jpg')",
     "url('./frontend/assets/lotusflower.jpg')",
     "url('./frontend/assets/vectoryflower.jpg')",
+    "url('./frontend/assets/4411.jpg')",
+    "url('./frontend/assets/2475484.jpg')",
+    "url('./frontend/assets/3464806.jpg')",
   ];
